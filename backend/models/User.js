3865c923f6d83194c637decdcf28e9d015a2aac2
Randormeur <r.pirate94@gmail.com>
2022-02-23 09:19:22 +0100

    Modification regex email

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,17 +1,10 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
-var validateEmail = function(email) {
-  var re = /^[^!@#$%^&*)(':;]+@[a-zA-Z]+\.[a-zA-Z]+$/;
-  return re.test(email)
-};
-var validatePassword = function(password) {
-  var re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
-  return re.test(password)
-};
+
 
 const userSchema = mongoose.Schema({
-  email: { type: String, required: true, unique: true, pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/},
+  email: { type: String, required: true, unique: true, pattern: /^[a-zA-Z0-9.!#$%&*+/=?^_~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/},
   password: {  type: String, required: true, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/}
 });
 
