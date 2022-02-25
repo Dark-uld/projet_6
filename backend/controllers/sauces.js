4c39b14f6bf9c65d10c9c1b01cb1414a9123dbf2
Randormeur <r.pirate94@gmail.com>
2022-02-25 03:43:29 +0100

    last commit de la session 24.02

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -42,3 +42,17 @@ exports.getOneSauce = (req, res, next) => {
     }
   );
 };
+
+exports.modifySauce = (req, res, next) => {
+  // test si il y a une nouvelle image
+  const sauceObject = req.file ?
+    {
+      // si fichier existe, on recup la nouvelle image
+      ...JSON.parse(req.body.sauce),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+      // sinon on reprend body
+    } : { ...req.body };
+  Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+};
