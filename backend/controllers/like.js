e875a5aa2143af64ca141894a8221c608f497e1f
Randormeur <r.pirate94@gmail.com>
2022-02-28 09:01:23 +0100

    ajout fonction dislike

--- backend/controllers/like.js
+++ backend/controllers/like.js
@@ -12,7 +12,7 @@ exports.modifyLike = (req, res, next) => {
       _id: req.params.id
     }).then(
       (sauce) => {
-        // on verifie si usier a deja like/dislike 
+        // Ajout d'un like 
         if (!sauce.usersLiked.includes(req.body.userId) && req.body.like === 1){
             Sauce
             .updateOne(
@@ -25,6 +25,7 @@ exports.modifyLike = (req, res, next) => {
             .then(() => res.status(200).json({ message: 'Like modifié !'}))
             .catch(error => res.status(400).json({ error })); 
         } 
+        // Retirer un like
         if (sauce.usersLiked.includes(req.body.userId) && req.body.like === 0){
             Sauce
             .updateOne(
@@ -32,6 +33,32 @@ exports.modifyLike = (req, res, next) => {
                 {
                     $inc: {likes: -1}, 
                     $pull : {usersLiked: req.body.userId}
+                }
+            )
+            .then(() => res.status(200).json({ message: 'Like modifié !'}))
+            .catch(error => res.status(400).json({ error })); 
+        } 
+        // Ajout d'un dislike 
+        if (!sauce.usersDisliked.includes(req.body.userId) && req.body.like === -1){
+            Sauce
+            .updateOne(
+                { _id: req.params.id }, 
+                {
+                    $inc: {dislikes: 1}, 
+                    $push : {usersDisliked: req.body.userId}
+                }
+            )
+            .then(() => res.status(200).json({ message: 'Like modifié !'}))
+            .catch(error => res.status(400).json({ error })); 
+        } 
+        // Retirer un dislike
+        if (sauce.usersDisliked.includes(req.body.userId) && req.body.like === 0){
+            Sauce
+            .updateOne(
+                { _id: req.params.id }, 
+                {
+                    $inc: {dislikes: -1}, 
+                    $pull : {usersDisliked: req.body.userId}
                 }
             )
             .then(() => res.status(200).json({ message: 'Like modifié !'}))
